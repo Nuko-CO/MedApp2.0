2175c3a8920a5bfe66c4ab7559e0603ded122023
Nuko-CO <75743680+Nuko-CO@users.noreply.github.com>
2024-12-15 22:20:50 +0500

    Initial Commit

---
+++ MedApp2.0/MedApp2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  MedApp2_0App.swift
+//  MedApp2.0
+//
+//  Created by Nurdaulet Darmenov on 12/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MedApp2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
